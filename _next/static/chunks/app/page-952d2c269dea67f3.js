d990a49456b9b07e21e6ab28ccccd399b29e9906
FelixZhang-1995 <60122884+FelixZhang-1995@users.noreply.github.com>
2026-08-27 14:11:56 +0800

    Give every companion skill real gameplay stats

--- _next/static/chunks/app/page-952d2c269dea67f3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{811:(e,s,_)=>{Promise.resolve().then(_.bind(_,369))}},e=>{e.O(0,[602,476,7,168,369,441,794,358],()=>e(e.s=811)),_N_E=e.O()}]);
